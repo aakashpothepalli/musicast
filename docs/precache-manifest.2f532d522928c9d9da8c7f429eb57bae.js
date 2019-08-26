f7cf27d7f96d71dd691dace1077cff8c3b345c67
saiaakash <p.saiaakash518@gmail.com>
2019-08-26 22:53:12 +0530

    setup

---
+++ docs/precache-manifest.2f532d522928c9d9da8c7f429eb57bae.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04384bae8406e3a31833a08748e141c1",
+    "url": "/musicast/index.html"
+  },
+  {
+    "revision": "d69a6859e4eaffbb827c",
+    "url": "/musicast/static/js/2.2a0309d5.chunk.js"
+  },
+  {
+    "revision": "c1ca0e7486140432c100",
+    "url": "/musicast/static/js/main.eb06f674.chunk.js"
+  },
+  {
+    "revision": "9bbf6674843bb1298f5c",
+    "url": "/musicast/static/js/runtime~main.32e94e76.js"
+  }
+]);
